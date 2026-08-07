f82f4642bc3a584beaa31e553dbc8c82ab93f499
lalthea <abacanlorenzalthea@gmail.com>
2026-08-07 14:27:34 +0800

    feat: added some installs for this.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <h1 className="text-3xl font-bold underline text-blue-500">
+            Hello world!
+        </h1>
+    </div>
   )
 }
 
