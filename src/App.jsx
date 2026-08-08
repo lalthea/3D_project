ae87e5efdac14c6a1235b9d0085d1e725d0662ef
lalthea <abacanlorenzalthea@gmail.com>
2026-08-08 15:26:10 +0800

    feat: added a navigation bars (home, about. projects, and contact)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import{ Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/NavBar";
 
 const App = () => {
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline text-blue-500">
-            Hello world!
-        </h1>
-    </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/projects" element={<h1>Projects</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
