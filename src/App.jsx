e4718d6ec9897589685674eab8d129cc68bb02a2
lalthea <abacanlorenzalthea@gmail.com>
2026-08-08 15:42:21 +0800

    feat: Added the 3d installer and minor feature in Home.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import{ Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar";
+import { Home, About, Projects, Contact } from "./components/pages";
 
 const App = () => {
   return (
@@ -7,10 +8,10 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/projects" element={<h1>Projects</h1>} />
-          <Route path="/contact" element={<h1>Contact</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
