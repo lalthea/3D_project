2bbc71d19eed06df555cde7676ed1aacddac9414
lalthea <abacanlorenzalthea@gmail.com>
2026-08-10 11:49:12 +0800

    chore: moving the page folder.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import {Suspense} from 'react'
 import {Canvas} from "@react-three/fiber";
-import Loader from '../Loader'
+import Loader from '../components/Loader'
 {/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         </div>*/}
 
